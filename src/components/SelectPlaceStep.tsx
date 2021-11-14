df69facf0a06557744fc5b939532f4ea8d608908
Corey Farwell <coreyf@rwell.org>
2021-11-13 23:40:30 -0500

    multiple locations per row during selection

--- src/components/SelectPlaceStep.tsx
+++ src/components/SelectPlaceStep.tsx
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import * as React from "react";
 import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
+import { Col, Row } from "react-bootstrap";
 
 export const SelectPlaceStep = ({
   places,
@@ -17,39 +18,43 @@ export const SelectPlaceStep = ({
       onSelectPlace(place);
     };
     return (
-      <Card key={i}>
-        <Card.Header>{place.display_name}</Card.Header>
-        <Card.Body>
-          <div className="d-grid gap-3" key={i}>
-            <div style={{ height: "150px", display: "flex" }}>
-              <MapContainer
-                attributionControl={false}
-                touchZoom={false}
-                tap={false}
-                keyboard={false}
-                boxZoom={false}
-                doubleClickZoom={false}
-                zoomControl={false}
-                dragging={false}
-                scrollWheelZoom={false}
-                center={[0, 0]}
-                zoom={5}
-                style={{ flexGrow: 1 }}
-              >
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <PlaceLayer place={place} />
-              </MapContainer>
+      <Col xs={12} sm={6} lg={4} key={i}>
+        <Card style={{marginBottom: "20px"}}>
+          <Card.Header>{place.display_name}</Card.Header>
+          <Card.Body>
+            <div className="d-grid gap-3" key={i}>
+              <div style={{ height: "150px", display: "flex" }}>
+                <MapContainer
+                  attributionControl={false}
+                  touchZoom={false}
+                  tap={false}
+                  keyboard={false}
+                  boxZoom={false}
+                  doubleClickZoom={false}
+                  zoomControl={false}
+                  dragging={false}
+                  scrollWheelZoom={false}
+                  center={[0, 0]}
+                  zoom={5}
+                  style={{ flexGrow: 1 }}
+                >
+                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                  <PlaceLayer place={place} />
+                </MapContainer>
+              </div>
+              <Button onClick={onClick}>Select</Button>
             </div>
-            <Button onClick={onClick}>Select</Button>
-          </div>
-        </Card.Body>
-      </Card>
+          </Card.Body>
+        </Card>
+      </Col>
     );
   });
 
   return (
     <Frame title="Places">
-      <div className="d-grid gap-3">{placesElems}</div>
+      <Row>
+        {placesElems}
+      </Row>
     </Frame>
   );
 };
